8d51742ea9fb2f1e5875f22221b97a79dc57d07a
Halis Basov <has.basov@gmail.com>
2026-05-21 13:20:00 +0300

    feat: export public API

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { Shape } from "./shapes/Shape.js";
+export { Rectangle } from "./shapes/Rectangle.js";
+export { Triangle } from "./shapes/Triangle.js";
+export { Circle } from "./shapes/Circle.js";
+export { ShapeType, ShapeEventMap, ShapeEvent } from "./types.js";
+
